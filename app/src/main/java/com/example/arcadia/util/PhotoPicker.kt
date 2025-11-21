33ba74b83c399892b015bf7f26a35d723cf02c1c
ahmed <ahmedbobos.rashid@gmail.com>
2025-11-21 20:17:23 +0200

    feat: Add PhotoPicker and SearchBar components with customizable options

---
+++ app/src/main/java/com/example/arcadia/util/PhotoPicker.kt
@@ -0,0 +1,44 @@
+package com.example.arcadia.util
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class PhotoPicker {
+    var openPhotoPicker by mutableStateOf(false)
+        private set
+
+    @Composable
+    fun InitializePhotoPicker(
+        onImageSelect: (Uri?) -> Unit
+    ) {
+        val pickMedia = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.PickVisualMedia()
+        ) { uri ->
+            onImageSelect(uri)
+            openPhotoPicker = false
+        }
+
+        LaunchedEffect(openPhotoPicker) {
+            if (openPhotoPicker) {
+                pickMedia.launch(
+                    PickVisualMediaRequest(
+                        ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            }
+        }
+    }
+
+    fun open() {
+        openPhotoPicker = true
+    }
+}
+
+
